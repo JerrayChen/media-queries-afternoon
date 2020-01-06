4d5122ae739c0534d55d3dc96bddb1f93dac53aa
Jerry Chen <raychen31638@gmail.com>
2020-01-06 17:11:26 -0600

    finished

---
+++ src/Components/Header.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './Header.css';
+class Header extends Component {
+    constructor() {
+        super();
+        this.state = {
+            menuStatus: "menu-status"
+        }
+    }
+
+    handleMenu(){
+        if(this.state.menuStatus === "menu-status-open"){
+            this.setState({
+                menuStatus: "menu-status-close"
+            });
+        }else{
+            this.setState({
+                menuStatus: "menu-status-open"
+            });
+        }
+    }
+
+    render() {
+        return (
+            <header>
+                <nav>
+                    <div className='logo'>
+                        Start Bootstrap
+                    </div>
+                    <ul className='desk-list'>
+                        <li className='menu-list-desktop'>SERVICES</li>
+                        <li className='menu-list-desktop'>PORTFOLIO</li>
+                        <li className='menu-list-desktop'>ABOUT</li>
+                        <li className='menu-list-desktop'>TEAM</li>
+                        <li className='menu-list-desktop'>CONTACT</li>
+                    </ul>
+                    <button className='menu-btn' onClick={()=> {this.handleMenu()}}>Menu☰</button>
+                    <ul className={this.state.menuStatus}>
+                        <li className='menu-list-mobile'>SERVICES</li>
+                        <li className='menu-list-mobile'>PORTFOLIO</li>
+                        <li className='menu-list-mobile'>ABOUT</li>
+                        <li className='menu-list-mobile'>TEAM</li>
+                        <li className='menu-list-mobile'>CONTACT</li>
+                    </ul>
+                </nav>
+                <main>
+                    <div className='welcome'>Welcome To Our Studio!</div>
+                    <div className='greeting'>IT'S NICE TO MEET YOU</div>
+                    <button className='more-info'>TELL ME MORE</button>
+                </main>
+
+            </header>
+        )
+    }
+}
+export default Header;
